7e5385032d8d1673c5dd27e90e82a7748f3c1bf7
Vicente Ibarra <vicenteantoibarramail.com>
2026-05-13 10:29:35 -0400

    me quiero matar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './paginas/login/login';
 import { FormularioAdopcion } from './paginas/formulario-adopcion/formulario-adopcion';
 import { CamposFormu } from './paginas/campos-formu/campos-formu';
+import { Solicitudes } from './paginas/solicitudes/solicitudes';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'formulario-adopcion', component: FormularioAdopcion},
   { path: 'campos-formu', component: CamposFormu},
+  { path: 'solicitudes', component: Solicitudes},
   { path:'**', redirectTo:''}
 
 ];
--- src/app/paginas/campos-formu/campos-formu.ts
+++ src/app/paginas/campos-formu/campos-formu.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-campos-formu',
@@ -9,123 +9,128 @@ import { Routes } from '@angular/router';
 })
 
 export class CamposFormu {
-  mascotaSeleccionada: any = null;
-  mascotas = [{
-    nombre: 'Luna',
-    especie: 'perro',
-    edad: '4 años',
-    sexo: 'hembra',
-    tamaño: 'mediano'
-  },
-  {
-    nombre:'Milo',
-    especie:'perro',
-    edad:'2 años',
-    sexo:'Macho',
-    tamaño:'Pequeño'
-  },
-  {
-    nombre:'Rocky',
-    especie:'Perro',
-    edad:'4 años',
-    sexo: 'Macho',
-    tamaño: 'Grande'
-  },
-  {
-    nombre:'Nala',
-    especie:'Gato',
-    edad:'3 años',
-    sexo:'Hembra',
-    tamaño:'Pequeño'
-  },
-  {
-    nombre: 'Toby',
-    especie: 'Perro',
-    edad:'6 meses',
-    sexo:'Macho',
-    tamaño:'mediano'
-  },
-  {
-    nombre:'Kira' ,
-    especie:'Perro',
-    edad:'5 años',
-    sexo:'Hembra',
-    tamaño:'Mediano'
-  },
-  {
-    nombre:'Max' ,
-    especie:'Perro',
-    edad:'1 año',
-    sexo:'Macho',
-    tamaño:'Grande'
-  },
-  {
-    nombre:'Pelusa' ,
-    especie:'Conejo',
-    edad:'8 meses',
-    sexo:'Hembra',
-    tamaño:'Pequeño'
-  },
-  {
-    nombre:'Simba' ,
-    especie:'Gato',
-    edad:'2 años',
-    sexo:'Macho',
-    tamaño:'Mediano'
-  },
-  {
-    nombre:'Canela' ,
-    especie:'Perro',
-    edad:'7 años',
-    sexo:'Hembra',
-    tamaño:'Mediano'
-  },
-  {
-    nombre:'Tom' ,
-    especie:'Gato',
-    edad:'4 años',
-    sexo:'Macho',
-    tamaño:'Mediano'
-  },
-  {
-    nombre:'Maya' ,
-    especie:'Perro',
-    edad:'3 años',
-    sexo:'Hembra',
-    tamaño:'Grande'
-  },
-  {
-    nombre:'Bruno',
-    especie:'Perro',
-    edad:'2 años',
-    sexo:'Macho',
-    tamaño:'Grande'
-  },
-  {
-    nombre:'Copito' ,
-    especie:'Conejo',
-    edad:'1 año',
-    sexo:'Macho',
-    tamaño:'Pequeño'
-  },
-  {
-    nombre:'Princesa' ,
-    especie:'Gato',
-    edad:'5 años',
-    sexo:'Hembra',
-    tamaño:'Pequeño'
-  }
-];
-  seleccionarMascota(event: Event) {
-  const select = event.target as HTMLSelectElement;
-  const nombreMascota = select.value;
+    mascotaSeleccionada: any = null;
+    mascotas = [{
+      nombre: 'Luna',
+      especie: 'perro',
+      edad: '4 años',
+      sexo: 'hembra',
+      tamaño: 'mediano'
+    },
+    {
+      nombre:'Milo',
+      especie:'perro',
+      edad:'2 años',
+      sexo:'Macho',
+      tamaño:'Pequeño'
+    },
+    {
+      nombre:'Rocky',
+      especie:'Perro',
+      edad:'4 años',
+      sexo: 'Macho',
+      tamaño: 'Grande'
+    },
+    {
+      nombre:'Nala',
+      especie:'Gato',
+      edad:'3 años',
+      sexo:'Hembra',
+      tamaño:'Pequeño'
+    },
+    {
+      nombre: 'Toby',
+      especie: 'Perro',
+      edad:'6 meses',
+      sexo:'Macho',
+      tamaño:'mediano'
+    },
+    {
+      nombre:'Kira' ,
+      especie:'Perro',
+      edad:'5 años',
+      sexo:'Hembra',
+      tamaño:'Mediano'
+    },
+    {
+      nombre:'Max' ,
+      especie:'Perro',
+      edad:'1 año',
+      sexo:'Macho',
+      tamaño:'Grande'
+    },
+    {
+      nombre:'Pelusa' ,
+      especie:'Conejo',
+      edad:'8 meses',
+      sexo:'Hembra',
+      tamaño:'Pequeño'
+    },
+    {
+      nombre:'Simba' ,
+      especie:'Gato',
+      edad:'2 años',
+      sexo:'Macho',
+      tamaño:'Mediano'
+    },
+    {
+      nombre:'Canela' ,
+      especie:'Perro',
+      edad:'7 años',
+      sexo:'Hembra',
+      tamaño:'Mediano'
+    },
+    {
+      nombre:'Tom' ,
+      especie:'Gato',
+      edad:'4 años',
+      sexo:'Macho',
+      tamaño:'Mediano'
+    },
+    {
+      nombre:'Maya' ,
+      especie:'Perro',
+      edad:'3 años',
+      sexo:'Hembra',
+      tamaño:'Grande'
+    },
+    {
+      nombre:'Bruno',
+      especie:'Perro',
+      edad:'2 años',
+      sexo:'Macho',
+      tamaño:'Grande'
+    },
+    {
+      nombre:'Copito' ,
+      especie:'Conejo',
+      edad:'1 año',
+      sexo:'Macho',
+      tamaño:'Pequeño'
+    },
+    {
+      nombre:'Princesa' ,
+      especie:'Gato',
+      edad:'5 años',
+      sexo:'Hembra',
+      tamaño:'Pequeño'
+    }
+  ];
+  constructor(private router:Router){}
 
-  this.mascotaSeleccionada = this.mascotas.find(
-    mascota => mascota.nombre === nombreMascota
-  );
+    seleccionarMascota(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    const nombreMascota = select.value;
 
-  console.log('Seleccionaste:', nombreMascota);
-  console.log('Datos:', this.mascotaSeleccionada);
-}
+    this.mascotaSeleccionada = this.mascotas.find(
+      mascota => mascota.nombre === nombreMascota
+    );
 
+    console.log('Seleccionaste:', nombreMascota);
+    console.log('Datos:', this.mascotaSeleccionada);
+  };
+  irSolicitudes(){
+    alert('Solicitud enviada')
+    this.router.navigate(['/solicitudes']);
+  };
 }
